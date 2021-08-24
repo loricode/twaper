b02291e86dd81b32f4e3e950cf30c5e9ac1ea036
loricode <ajcampillo07@gmail.com>
2021-08-24 00:53:09 -0500

    trabajando en vista movil

--- src/app/web/components/home-content-organize-show/home-content-organize-show.component.ts
+++ src/app/web/components/home-content-organize-show/home-content-organize-show.component.ts
@@ -1,11 +1,32 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, HostListener } from '@angular/core';
 
 @Component({
   selector: 'app-home-content-organize-show',
   templateUrl: './home-content-organize-show.component.html',
   styleUrls: ['./home-content-organize-show.component.css']
 })
-export class HomeContentOrganizeShowComponent {
+export class HomeContentOrganizeShowComponent implements OnInit {
+
+
+  cellsToShow:number = 5
 
   constructor() { }
+
+  ngOnInit(): void {
+    if(window.innerWidth <= 300){
+      this.cellsToShow = 2;
+    }
+  }
+
+@HostListener('window:resize', ['$event'])
+onResize(event:any) {
+   
+   if(window.innerWidth <= 300){
+     this.cellsToShow = 2;
+   }else if(window.innerWidth > 1040){
+    this.cellsToShow = 5;
+   }
+
+}
+
 }
